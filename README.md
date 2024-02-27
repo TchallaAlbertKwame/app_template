69213c659c550166a6c98f58c94ca3037d381c15
TchallaAlbertKwame <tchallaalbertkwame@gmail.com>
2024-02-27 13:23:13 +0000

    Initial commit of the repo

---
+++ README.md
@@ -0,0 +1,14 @@
+# app_template
+
+A #Flutter #template that can be used for all your flutter #projects. You can modify to suit you use case. I normally prefer to go by this approach.
+
+## Getting Started
+
+Clone the repo:  [https://github.com/TchallaAlbertKwame/app_template.git](https://github.com/TchallaAlbertKwame/app_template.git)
+## Run:  
+_* flutter clean  
+_* flutter pub get   
+_* flutter run  
+### Enjoy flutter development!
+
+
---
+++ lib/constants/styles.dart
@@ -0,0 +1,57 @@
+//  The styles used throughout the app is play in here. So you don't have to put the styling directly in your code.
+
+import 'package:flutter/material.dart';
+
+const kMainFont = 'Roboto'; //The main font of the app
+const kButtonFont = 'Roboto';
+const kDisplayFont = 'SquadaOne';
+
+const Color kWhiteColor = Color(0xFFFFFFFF);
+const Color kGreenColor = Color(0xFF3B742C);
+const Color kBlackColor = Color(0xFF0A0702);
+const Color kBrownColor = Color(0xFF5C1211);
+const Color kDefaultAppColor = Color(0xFFFF3333); //Default color of the app
+const Color kTextColor = Color(0xFF000000);
+// const Color kTextColor = Color(0xFF);
+
+const kColorGreen = Color(0xFF395144);
+const kColorLightGreen = Color(0XFF4E6C50);
+const kColorBrown = Color(0XFFAA8B56);
+const kColorLightYellow = Color(0xFFF0EBCE);
+
+const kColorRed = Color(0xFFD96666);
+const kColorLightRed = Color(0xFFF2CECE);
+const kColorLightGray = Color(0xFFDDDDDD);
+
+const kColorHunterGreen = Color(0xFF386641);
+const kColorMayGreen = Color(0xFF6a994e);
+const kColorAndroidGreen = Color(0xFFa7c957);
+const kColorEggshell = Color(0xFFf2e8cf);
+const kColorBitterSweetShimmer = Color(0xFFbc4749);
+
+const kBgColor = kColorGreen;
+
+const kTitleTextStyle = TextStyle(
+  fontFamily: kDisplayFont,
+  fontSize: 50.0,
+  color: kColorAndroidGreen,
+  decoration: TextDecoration.none,
+);
+
+const kAnalyzingTextStyle = TextStyle(
+    fontFamily: kMainFont,
+    fontSize: 25.0,
+    color: kColorEggshell,
+    decoration: TextDecoration.none);
+
+const kResultTextStyle = TextStyle(
+    fontFamily: kDisplayFont,
+    fontSize: 35.0,
+    color: kColorLightYellow,
+    decoration: TextDecoration.none);
+
+const kResultRatingTextStyle = TextStyle(
+    fontFamily: kMainFont,
+    fontSize: 18.0,
+    color: Colors.white,
+    decoration: TextDecoration.none);
